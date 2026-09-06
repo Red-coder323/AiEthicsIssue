14cf426ffde45d38a935701e043c3e703f9a8b11
Red-coder323 <mjfd050514@gmail.com>
2026-09-06 09:16:15 +0800

    Dark Mode test

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
+const wantdark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+function toggledm() {
+    const html = document.documentElement;
+    html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
+}
+
 function main() {
-    
+    if (wantdark) {
+        document.documentElement.dataset.theme = 'dark';
+    }
 }
 main();
