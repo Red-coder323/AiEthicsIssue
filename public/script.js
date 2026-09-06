104bcd270596b32972c29bd55befb43d675ef696
Red-coder323 <mjfd050514@gmail.com>
2026-09-06 09:37:09 +0800

    Duron

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 const wantdark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 function toggledarkmode() {
-    document.documentElement.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
+    const html = document.documentElement;
+    html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
 }
 
 function main() {
